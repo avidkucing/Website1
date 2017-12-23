4ec8c2b5858aad179ee875f56e1807fc99aa5acc
avidsantiko <avidkucing@gmail.com>
2017-12-24 00:56:22 +0700

    Impovements
    -Added the javascript when changing tab
    -Added link to table row
    -Added print-ltb page to be the target of table row link
    -Added some icons

---
+++ gudang.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$("tbody tr").addClass("klik");
+	$(".klik").attr("data-href","print-lpb.html");
+	$(".klik").click(function() {
+        window.location = $(this).data("href");
+    });
+	$("#bahanbakutab").click(function(){
+		$("#bahanjaditab").removeClass("active");
+		$("#bahanbakutab").addClass("active");
+		$(".bahanjadi").hide();
+		$(".bahanbaku").fadeIn("fast");
+	});
+	$("#bahanjaditab").click(function(){
+		$("#bahanbakutab").removeClass("active");
+		$("#bahanjaditab").addClass("active");
+		$(".bahanbaku").hide();
+		$(".bahanjadi").fadeIn("fast");
+	});
+});	    
