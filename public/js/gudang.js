29a20b4cfb38c17d87fdafb531046237d5f463b0
avidsantiko <avidkucing@gmail.com>
2017-12-30 21:05:08 +0700

    Cursor tangan

--- public/js/gudang.js
+++ public/js/gudang.js
@@ -18,6 +18,22 @@ $(document).ready(function(){
         });*/
     });
 
+    $('#print').click(function () {
+                var doc = new jsPDF();
+                var specialElementHandlers = {
+                    '.button-container': function (element, renderer) {
+                        return true;
+                    }
+                };
+   
+                doc.fromHTML($('body').get(0), 15, 15, {
+                    'width': 170,
+                    'elementHandlers': specialElementHandlers
+                });
+                doc.save('sample-file.pdf');
+            });
+
+
     $("#bahanbakutab").click(function(){
 		$("#bahanjaditab").removeClass("active");
 		$("#bahanbakutab").addClass("active");
