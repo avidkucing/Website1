449d9f4fbf5064ab1ce102921fc941546f23f2ad
muhabibull <tanteihabib55@gmail.com>
2018-02-24 11:29:05 +0700

    show stock bahan bantu

--- application/models/Data_stock.php
+++ application/models/Data_stock.php
@@ -43,12 +43,12 @@ Class Data_stock extends CI_Model {
  	}
 
  	public function stock_bantu(){
- 		$this->db->select('analisa_sampel.Nomor_Analisa, jenis_bahan.Kode_Bahan, bahan_terima.Nama_Manufacturer, nomor_batch_bahan.EXP_Date, nomor_batch_bahan.Jumlah, nomor_batch_bahan.Keterangan, bahan_terima.Nomor_LPB, nomor_batch_bahan.ID_Batch');
+ 		$this->db->select('jenis_bahan.Kode_Bahan, bahan_terima.Nama_Manufacturer, nomor_batch_bahan.EXP_Date, nomor_batch_bahan.Jumlah, nomor_batch_bahan.Keterangan, bahan_terima.Nomor_LPB, nomor_batch_bahan.ID_Batch');
  		$this->db->from('nomor_batch_bahan');
  		$this->db->join('bahan_terima', 'bahan_terima.Nomor_LPB = nomor_batch_bahan.Nomor_LPB', 'inner');
- 		$this->db->join('analisa_sampel', 'nomor_batch_bahan.ID_Batch = analisa_sampel.ID_Batch', 'inner');
+ 		//$this->db->join('analisa_sampel', 'nomor_batch_bahan.ID_Batch = analisa_sampel.ID_Batch', 'inner');
  		$this->db->join('jenis_bahan', 'bahan_terima.ID_Bahan = jenis_bahan.ID_Bahan', 'inner');
- 		$this->db->order_by('Nomor_Analisa desc');
+ 		//$this->db->order_by('Nomor_Analisa desc');
  		$this->db->where("nomor_batch_bahan.Status", "RELEASE");
  		$this->db->where("jenis_bahan.Jenis", "Pembantu");
  		
